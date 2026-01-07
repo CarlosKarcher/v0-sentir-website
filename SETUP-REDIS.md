9d5a26234f6c39b436d5b3475860ee1c106f5f6b
Carlos Karcher <carlos@sentir.com>
2026-01-07 13:42:59 -0300

    Cambiar nombre del menÃº 'Merchandising' a 'Tienda Sentir'

--- SETUP-REDIS.md
+++ SETUP-REDIS.md
@@ -65,3 +65,4 @@ REDIS_URL="redis://default:2UfHuxpPPSJq39ZAgLoYtkSyayR3fdaU@redis-12308.c10.us-e
 
 
 
+
--- components/header.tsx
+++ components/header.tsx
@@ -252,7 +252,7 @@ export function Header() {
                     "group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 cursor-pointer",
                   )}
                 >
-                  Merchandising
+                  Tienda Sentir
                 </NavigationMenuLink>
               </NavigationMenuItem>
             </NavigationMenuList>
@@ -335,7 +335,7 @@ export function Header() {
                   }, 100)
                 }}
               >
-                Merchandising
+                Tienda Sentir
               </a>
             </nav>
           </SheetContent>

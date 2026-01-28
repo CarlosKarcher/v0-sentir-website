47c806bd8ed5e522b3c10518be90da430d73f0bd
Carlos Karcher <carlos@sentir.com>
2026-01-28 10:36:13 -0300

    Corregir nombre exacto del flyer de Biodecodificacion

--- components/events.tsx
+++ components/events.tsx
@@ -221,8 +221,8 @@ export function Events() {
       available: true,
       availabilityText: "Cupos disponibles",
       hasFlyer: true,
-      flyerImage: "/Taller-de-Bio-07-03-2026. .JPEG",
-      flyerImageAlt: "/images/Taller-de-Bio-07-03-2026. .JPEG",
+      flyerImage: "/Taller-de-Bio-07-03-2026.jpeg",
+      flyerImageAlt: "/images/Taller-de-Bio-07-03-2026.jpeg",
       contactPhone: "+54 9 2966 211547",
     },
     {

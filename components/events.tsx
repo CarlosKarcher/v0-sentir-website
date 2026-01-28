38e06168009dde21298568969e49ea115fa37ca8
Carlos Karcher <carlos@sentir.com>
2026-01-28 10:08:19 -0300

    Corregir extension del flyer de Biodecodificacion a .JPEG

--- components/events.tsx
+++ components/events.tsx
@@ -212,8 +212,8 @@ export function Events() {
       available: true,
       availabilityText: "Cupos disponibles",
       hasFlyer: true,
-      flyerImage: "/Taller de Bio Rio Gallegos 07-03-2026.jpeg",
-      flyerImageAlt: "/images/Taller de Bio Rio Gallegos 07-03-2026.jpeg",
+      flyerImage: "/Taller de Bio Rio Gallegos 07-03-2026.JPEG",
+      flyerImageAlt: "/images/Taller de Bio Rio Gallegos 07-03-2026.JPEG",
       contactPhone: "+54 9 2966 211547",
     },
     {
